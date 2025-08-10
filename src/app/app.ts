0dd0a9f8f9b9061eacb3ca82166007142eaabe86
Kephotho Solutions <30622305+kephothoX@users.noreply.github.com>
2025-08-10 14:16:55 +0300

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,24 @@
 import { Component, signal, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 import { HederaFungibleToken } from './hedera-fungible-token';
 import { AppService } from './app-service';
 import { HeaderComponent } from './components/header/header.component';
 import { AccountInfoQuery, TopicCreateTransaction } from '@hashgraph/sdk';
+import { Account } from './models/account';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -18,12 +26,28 @@ import { AccountInfoQuery, TopicCreateTransaction } from '@hashgraph/sdk';
 export class App implements OnInit {
   protected readonly title = signal('MDQ Bids');
 
+  GetAcctCredsFrmSessStorage: string = JSON.parse(JSON.stringify(`${window.sessionStorage.getItem('LoginAccountCredentials')}`));
+
+  AccountCredentials: Account = this.GetAcctCredsFrmSessStorage ? JSON.parse(this.GetAcctCredsFrmSessStorage) : { account_id: '', private_key: '', evm_address: '' };
+
+
   constructor(
     private _appService: AppService,
+    private _router: Router,
     private _hederaFungibleToken: HederaFungibleToken
   ) { }
 
   async ngOnInit(): Promise<void> {
+    if (!this.AccountCredentials.account_id || !this.AccountCredentials.private_key || !this.AccountCredentials.evm_address) {
+      this._router.navigate(['/login']);
+    } else {
+      window.sessionStorage.setItem('HederaAccountID', this.AccountCredentials.account_id);
+      window.sessionStorage.setItem('HederaPrivateKey', this.AccountCredentials.private_key);
+      window.sessionStorage.setItem('EVMAddress', this.AccountCredentials.evm_address);
+
+      console.log('Account Credentials', this.AccountCredentials);
+    }
+
     const accountInfoQuery = new AccountInfoQuery()
       .setAccountId(`${window.sessionStorage.getItem('HederaAccountID')}`);
 
